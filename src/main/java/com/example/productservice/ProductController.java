33519e2cdaba71aade179fffe3ce1ede06fa4997
Zainab Alayande <alayandezainab64@gmail.com>
2025-11-25 18:10:58 +0100

    product service

---
+++ src/main/java/com/example/productservice/ProductController.java
@@ -0,0 +1,15 @@
+package com.example.productservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("/products")
+    public String getProducts() {
+        return "List of products from PRODUCT-SERVICE";
+    }
+
+
+}
